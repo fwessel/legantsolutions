ed92c1ca896db20520375506c8f94b136e2fbf10
David Overman <portalerd@icloud.com>
2025-04-21 22:44:42 -0700

    Add diagnostics page

--- src/app/diagnostics/diagnostics.component.ts
+++ src/app/diagnostics/diagnostics.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from "../page-container/page-container.component";
 
 @Component({
   selector: 'app-diagnostics',
-  imports: [],
+  imports: [PageContainerComponent],
   templateUrl: './diagnostics.component.html',
   styleUrl: './diagnostics.component.css'
 })
