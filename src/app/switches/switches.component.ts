046228228e40364d0d7f46c13d57ccfdafe96a84
David Overman <portalerd@icloud.com>
2025-04-22 15:19:26 -0700

    Add switches content

--- src/app/switches/switches.component.ts
+++ src/app/switches/switches.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from "../page-container/page-container.component";
+import { ShowcaseComponent } from '../showcase/showcase.component';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-switches',
-  imports: [],
+  imports: [PageContainerComponent, ShowcaseComponent],
   templateUrl: './switches.component.html',
   styleUrl: './switches.component.css'
 })
 export class SwitchesComponent {
+  constructor(public navService: NavigationService) {}
 
 }
