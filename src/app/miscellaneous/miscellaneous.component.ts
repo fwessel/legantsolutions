bbae34dbaaa8b356ce2ed4457bd66fb266092d19
David Overman <portalerd@icloud.com>
2025-04-22 15:42:03 -0700

    Add misc content

--- src/app/miscellaneous/miscellaneous.component.ts
+++ src/app/miscellaneous/miscellaneous.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from '../page-container/page-container.component';
+import { ShowcaseComponent } from '../showcase/showcase.component';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-miscellaneous',
-  imports: [],
+  imports: [PageContainerComponent, ShowcaseComponent],
   templateUrl: './miscellaneous.component.html',
   styleUrl: './miscellaneous.component.css'
 })
 export class MiscellaneousComponent {
-
+  constructor (public navService: NavigationService) {}
+  
 }
--- src/app/transformers/transformers.component.ts
+++ src/app/transformers/transformers.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from '../page-container/page-container.component';
+import { ShowcaseComponent } from '../showcase/showcase.component';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-transformers',
-  imports: [],
+  imports: [PageContainerComponent, ShowcaseComponent],
   templateUrl: './transformers.component.html',
   styleUrl: './transformers.component.css'
 })
 export class TransformersComponent {
+  constructor(public navService: NavigationService) {}
 
 }
