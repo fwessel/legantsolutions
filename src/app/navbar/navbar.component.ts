b3a30112dd90a9b59dc94abf2530645e38d38a76
David Overman <portalerd@icloud.com>
2025-04-22 16:46:36 -0700

    Improve navbar dropdown menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { NavigationService } from '../navigation.service';
 
@@ -9,7 +9,26 @@ import { NavigationService } from '../navigation.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isComponentsDropdownOpen = false;
+
+  @ViewChild('componentsDropdownMenu') componentsDropdownMenu !: ElementRef;
+  @ViewChild('componentsDropdownBtn') componentsDropdownBtn !: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  onComponentsClick(event: MouseEvent) {
+    this.onComponentsClickAndHover(event);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onComponentsHover(event: MouseEvent) {
+    this.onComponentsClickAndHover(event);
+  }
+
+  onComponentsClickAndHover(event: MouseEvent) {
+    this.isComponentsDropdownOpen = 
+      this.componentsDropdownMenu?.nativeElement.contains(event.target)
+      || this.componentsDropdownBtn?.nativeElement.contains(event.target)
+  }
+
   constructor(public navService: NavigationService) {}
-  
-  
 }
