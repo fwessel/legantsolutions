0b4ef458ab91e849850ff838aebcef195926bf77
David Overman <portalerd@icloud.com>
2025-04-23 22:06:22 -0700

    Add contact content

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from "../page-container/page-container.component";
+import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [PageContainerComponent, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  submitted = false;
+  contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    company: new FormControl(''),
+    phone: new FormControl(''),
+    reason: new FormControl(''),
+    message: new FormControl(''),
+  });
 
+  onSubmit() {
+    this.submitted = true;
+    this.contactForm.reset();
+  }
 }
